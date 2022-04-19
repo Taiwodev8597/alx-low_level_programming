2fc4ce184e0d9e7d1dedf50904a297154c6e23d4
Taiwodev8597 <oladotun.oladebo@yahoo.com>
2022-04-18 17:17:02 -0700

    C functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#insert "main.h"
+/**main - Entry point
+ * Description: program to print alphabets in lower case
+ * return: nothing
+ */
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n')
+}
